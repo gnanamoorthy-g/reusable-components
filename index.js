872e74070cfe2ace649f83dc351a4987b1cc9d1d
gnanamoorthy-g <ggnanamoorthy215@gmail.com>
2023-12-05 18:28:18 +0530

    added accordion component

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import  { createRoot }  from 'react-dom/client';
 import App from './src/App';
 import './src/styles/index.scss'
+import './src/styles/main.css'
 
 const container = document.getElementById('root');
 const root = createRoot(container);
